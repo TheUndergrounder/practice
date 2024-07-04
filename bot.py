5b860c2f6e51c4172f30566888beaddd146752a7
Arthur Naumov <arturnaumov789@gmail.com>
2024-07-04 19:16:31 +0300

    button

--- bot.py
+++ bot.py
@@ -2,7 +2,9 @@ import asyncio
 import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
-from config_reader import config
+from aiogram import F
+from base import *
+#from config_reader import config
 
 # Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
@@ -11,11 +13,29 @@ bot = Bot(token="7265948579:AAGRZ1rwfn19i9KUYzukR7U8M5WfEoXT1ko")
 # Диспетчер
 dp = Dispatcher()
 
-# Хэндлер на команду /start
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
-    await message.answer("Hello!")
+    kb = [
+        [
+            types.KeyboardButton(text="Преподаватели"),
+            types.KeyboardButton(text="Аудитории")
+        ],
+    ]
+    keyboard = types.ReplyKeyboardMarkup(
+        keyboard=kb,
+        resize_keyboard=True,
+        input_field_placeholder="Выберите способ подачи"
+    )
+    await message.answer("Расписание", reply_markup=keyboard)
 
+@dp.message(F.text.lower() == "преподаватели")
+async def cmd_prep(message: types.Message):
+    await message.reply("Выберите преподавателя\n"+list_prep())
+
+@dp.message(F.text.lower() == "аудитории")
+async def cmd_audit(message: types.Message):
+    await message.reply("Расписание аудиторий")
+    
 # Хэндлер на команду /test1
 @dp.message(Command("test1"))
 async def cmd_test1(message: types.Message):
