3f3abc120fcc698179ef2a3920534b0ab0d982fa
Sasha Rudnev <alex.rudnevvv@gmail.com>
2024-07-05 05:13:58 +0300

    пары Яблокова с инфой

--- pars.py
+++ pars.py
@@ -1,5 +1,58 @@
 import requests
 from bs4 import BeautifulSoup
+
+
+def schedule_of_teachers(*, link: str, teacher: str)->dict:
+    schedule_of_teacher = {}
+    responce = requests.get(link).text
+    soup = BeautifulSoup(responce, 'lxml')
+    html_cod = str(soup.find(name='div', class_='result'))
+    while '<h3>' in html_cod:
+        index_h3=html_cod.find('<h3>')
+        html_cod=html_cod[index_h3+4:]
+        index_h3_next = html_cod.find('<h3>')
+        index_day = html_cod.find('<')
+        day_of_the_week=html_cod[:index_day]#Понедельник
+        cod_of_day=html_cod[:index_h3_next]
+        print(day_of_the_week)
+        while '<h4>'in cod_of_day:
+            index_h4 = cod_of_day.find('<h4>')
+            cod_of_day = cod_of_day[index_h4 + 4:]
+            index_h4_next = cod_of_day.find('<h4>')
+            index_lesson = cod_of_day.find('<')
+            num_of_lesson = cod_of_day[:index_lesson] #5 пара (16:40–18:10)
+            cod_of_lesson=cod_of_day[:index_h4_next]
+            up_or_down="верхняя и нижняя"
+            if "title" in cod_of_lesson:
+                index_up_or_down=cod_of_lesson.find('title')+7
+                end_of_index_up_or_down=cod_of_lesson[index_up_or_down:].find('"')
+                up_or_down=cod_of_lesson[index_up_or_down:index_up_or_down+end_of_index_up_or_down]#верхняя (нечетная)
+                cod_of_lesson=cod_of_lesson[index_up_or_down+end_of_index_up_or_down:]
+
+            index_type_of_lesson=cod_of_lesson.find('<b>')+3
+            end_of_index_of_type=cod_of_lesson[index_type_of_lesson:].find('</b>')
+            type_of_lesson=cod_of_lesson[index_type_of_lesson:index_type_of_lesson+end_of_index_of_type]#Л (или лр)
+
+            cod_of_lesson=cod_of_lesson[index_type_of_lesson+end_of_index_of_type:]
+            index_of_object=cod_of_lesson.find('–')
+            end_of_index_of_object=cod_of_lesson.find('<em>')
+            object=cod_of_lesson[index_of_object:end_of_index_of_object]#– Электроника
+
+            cod_of_lesson=cod_of_lesson[end_of_index_of_object:]
+            index_of_adress=cod_of_lesson.find('–')
+            end_of_index_of_adress=cod_of_lesson.find('</em>')
+            adress=cod_of_lesson[index_of_adress:end_of_index_of_adress]#– Гастелло 15, ауд. 31-02
+
+            index_groups=cod_of_lesson.find('Группы:')+8
+            cod_of_lesson=cod_of_lesson[index_groups:]
+            soup = BeautifulSoup(cod_of_lesson, 'lxml')
+            cod_of_groups=soup.find_all('a')
+            groups=[]#['1241', '1242', '1243', '1245', '7241']
+            for group in cod_of_groups:
+                groups.append(group.text)
+            schedule_of_teacher[teacher, day_of_the_week, num_of_lesson]=[up_or_down,type_of_lesson,object, adress,groups]
+
+    return schedule_of_teacher
 def find_id_teachers(*, link: str):
     responce=requests.get(link).text
     soup=BeautifulSoup(responce, 'lxml')
@@ -22,8 +75,8 @@ def find_id_teachers(*, link: str):
                 teachers_id = block[start + 1:end]
                 teachers_id_from_14.append(teachers_id)
     for teacher_id in teachers_id:
-        print(teacher_id)
-
+        link_with_id='https://guap.ru/rasp/?p='+str(teacher_id)
+        #function
 
 def write_teachers_from_14_in_file(*, link: str):
     responce = requests.get(link).text
@@ -40,7 +93,10 @@ def write_teachers_from_14_in_file(*, link: str):
                 file.write(surname)
 def main():
     #write_teachers_from_14_in_file(link="https://new.guap.ru/i01/k14#tab_k14_2")
-    find_id_teachers(link="https://guap.ru/rasp/")
-
+    #find_id_teachers(link="https://guap.ru/rasp/")
+    shedule=schedule_of_teachers(link='https://guap.ru/rasp/?p=' + '675', teacher='Яблоков')
+    print(shedule)
+    #print(shedule)
+    #675 - Яблоков
 if __name__=="__main__":
     main()
